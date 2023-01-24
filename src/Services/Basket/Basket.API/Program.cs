e1e2f278f627b2d78e39fb157af3c6b9bb901bbf
AndriiFil <fav80@ukr.net>
2023-01-24 23:23:14 +0200

    Discount.Grpc and Basket.API

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,6 @@
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.OpenApi.Models;
 using System.Runtime.CompilerServices;
 
@@ -10,7 +12,12 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();  
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(
+    o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
+
+builder.Services.AddScoped<DiscountGrpcService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
